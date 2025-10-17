fc5099ec80928dfb42af8d8c52e1d5a71966bb25
Ikhsan Fahri Gultom <ikhsanfahrigultom@gmail.com>
2025-10-17 19:11:35 +0700

    check

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,13 @@
-// backend/src/routes/authRoutes.js
 const express = require("express");
-const authController = require("../controllers/authController"); // Path disesuaikan
+const authController = require("../controllers/authController"); 
 
-const { authenticateToken } = require("../middleware/authMiddleware"); // Path disesuaikan
+const { authenticateToken } = require("../middleware/authMiddleware"); 
 const router = express.Router();
 
 
 router.put("/change-password", authenticateToken, authController.changePassword);
-// /api/auth/login
 router.post("/login", authController.login);
 
-// /api/auth/me
 router.get("/me", authenticateToken, authController.getCurrentUser);
 
 
